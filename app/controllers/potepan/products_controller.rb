7d8e7bf91f202aed913071fe60860485e0ffbbd5
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-09-02 19:17:24 +0900

    app/views/layouts/_head.html.erbの<title>BIGBAG Store</title>の削除 layouts/application.html.erbを_head.html.erbパーシャルにまとめる views/potepan/products/show.html.erbを -<div class="item <%= "active" if i == 0 %>" data-thumb="<%= i %>"> +<div class="item <%= "active" if i.zero? %>" data-thumb="<%= i %>">に変更 config/environments/development.rbを -  config.eager_load = true +  config.eager_load = falseに変更 spec/controllers/potepan/products_controller_spec.rbを動くように修正

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,6 +2,5 @@ class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
     @images = @product.images
-    
   end
 end
--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Potepan::ProductsController, type: :controller do
-    describe 'Get #show' do
-      before do
-        @product = create(:product)
-        get :show, params: {id: @product.id}
-      end
-      it 'リクエストは200 OKとなること' do
-        expect(response.status).to eq 200
-      end
-      it '@productに適切なプロダクトを割り当てること' do
-        expect(assigns(:product)).to eq @product
-      end
-      it ':showテンプレートを表示すること' do
-        expect(response).to render_template :show
-      end
+  describe 'Get #show' do
+    before do
+      @product = create(:product)
+      get :show, params: {id: @product.id}
+    end
+    it 'リクエストは200 OKとなること' do
+      expect(response.status).to eq 200
+    end
+    it '@productに適切なプロダクトを割り当てること' do
+      expect(assigns(:product)).to eq @product
+    end
+    it ':showテンプレートを表示すること' do
+      expect(response).to render_template :show
     end
   end
 end
