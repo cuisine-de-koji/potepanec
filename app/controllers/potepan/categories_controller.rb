7e6f083d886fb331eeb5011d9f0b371a6aa4b161
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-09-23 19:57:20 +0900

    update categories_controller update sidebar.html update show.html

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,4 +1,17 @@
 class Potepan::CategoriesController < ApplicationController
   def show
+    @taxon = Spree::Taxon.find(params[:id])
+    @products = show_products(@taxon)
+    @roots_taxon = Spree::Taxon.roots
   end
+
+    private
+      def show_products(taxon)
+        if taxon.root?
+          taxon.leaves.includes(:products).map(&:products).flatten.compact.uniq
+        else
+          taxon.products
+        end
+      end
+
 end
