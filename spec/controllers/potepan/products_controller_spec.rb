6b9e8471e5c205158a4bafa9429d151a5523c357
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-09-30 13:37:57 +0900

    clean product_contoroller_spec

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -11,9 +11,11 @@ RSpec.describe Potepan::ProductsController, type: :controller do
     it 'リクエストは200 OKとなること' do
       expect(response.status).to eq 200
     end
+
     it '@productに適切なプロダクトを割り当てること' do
       expect(assigns(:product)).to eq product
     end
+
     it ':showテンプレートを表示すること' do
       expect(response).to render_template :show
     end
