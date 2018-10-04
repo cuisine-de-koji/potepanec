5fda9d69da57b38175fd5f759d3b3f0128b5b650
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-10-04 16:16:32 +0900

    shared/_partners_logo.html.erbの共通化 helperメソッドのテストを追加

--- spec/controllers/potepan/categories_controller_spec.rb
+++ spec/controllers/potepan/categories_controller_spec.rb
@@ -3,10 +3,22 @@ require 'rails_helper'
 RSpec.describe Potepan::CategoriesController, type: :controller do
 
   describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+
+    let(:taxon) { create :taxon }
+    before do
+      get :show, params: {id: taxon.id}
+    end
+
+    it 'リクエストは200 OKとなること' do
+      expect(response.status).to eq 200
+    end
+
+    it '@taxonに適切なプロダクトを割り当てること' do
+      expect(assigns(:taxon)).to eq taxon
+    end
+
+    it ':showテンプレートを表示すること' do
+      expect(response).to render_template :show
     end
   end
-
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe 'full_titleヘルパーメソッドのテスト' do
+
+    context "page_titleが空の場合" do
+      it "base_titleだけ表示されること" do
+        expect(helper.full_title('')).to eq('BIGBAG Store')
+      end
+    end
+
+    context "page_titleが空でない場合" do
+      it "page_titleとbase_titleが表示されること" do
+        expect(helper.full_title('PageTitle')).to eq('PageTitle | BIGBAG Store')
+      end
+    end
+
+  end
+end
