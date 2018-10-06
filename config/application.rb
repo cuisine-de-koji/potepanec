7249a9a60c28ce89b88e2958303f26c7b990ad9b
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-10-06 17:55:26 +0900

    clean application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,19 +18,6 @@ Bundler.require(*Rails.groups)
 
 module Potepanec
   class Application < Rails::Application
-    
-    config.to_prepare do
-      # Load application's model / class decorators
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-
-      # Load application's view overrides
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-    end
-
 
     config.to_prepare do
       # Load application's model / class decorators
@@ -43,7 +30,6 @@ module Potepanec
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
