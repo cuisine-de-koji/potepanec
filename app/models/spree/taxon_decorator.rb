78be264429b8e2aed517d3b8ca271d615ad27766
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-10-19 19:02:28 +0900

    update categories_controller_spec.rb, clean products_controller_spec.rb,taxon_decorator.rb and _side_bar.html.erb
    categoriesコントローラーの@productsのテストの追加と、その他整理

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -3,7 +3,7 @@ Spree::Taxon.class_eval do
     #taxonがrootノードかどうかで表示商品を場合分け
     if root?
       #子供を持たない末端の葉っぱtaxonsのAR(ActiveRecord::Relation)商品群を取得後,
-      #重複がなくユニークでnilや空白を削除した配列に整形
+      #重複がなくユニークで且つnilや空白を削除した配列に整形
       leaves.includes(:products).map(&:products).flatten.compact.uniq
     else
       products
--- spec/controllers/potepan/categories_controller_spec.rb
+++ spec/controllers/potepan/categories_controller_spec.rb
@@ -1,25 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Potepan::CategoriesController, type: :controller do
-  describe "GET #show" do
-    #solidus/core/lib/spree/testing_support/factories/taxonomy_factory.rbにあるfactoryを呼び出している
-    binding.pry
-    let(:taxon) { create :taxon }
-
-    before do
-      get :show, params: { id: taxon.id }
+  describe '#show' do
+    let(:taxonomy) { create(:taxonomy) }
+    let(:apple) do
+      taxonomy.root.children.create(name: 'Apple', taxonomy: taxonomy)
+    end
+    #上記の様な形で:apple(spree_taxon)を作るのはtaxon_decoratorの#show_productsで'leaves'メソッドを使用しているから。:appleがrootノードになってしまうと、if分岐で'leaves'が働き、自身(:apple)を除いたproductsを返すので戻り値がnilになる。
+    let(:products_list) do
+      %w(air pro mini).map do |name|
+        create(:product, name: "#{name}") do |product|
+          product.taxons << apple
+        end
+      end
     end
 
-    it 'リクエストは200 OKとなること' do
-      expect(response.status).to eq 200
+    before do
+      get :show, params: { id: apple.id }
+    end
+
+    it 'responds successfully' do
+      expect(response).to be_success
     end
 
     it '@taxonに適切なカテゴリーを割り当てること' do
-      expect(assigns(:taxon)).to eq taxon
+      expect(assigns(:taxon)).to eq apple
     end
 
-    it ':showテンプレートを表示すること' do
-      expect(response).to render_template :show
+    it '@taxonに属する製品が正しく割り当てられている' do
+      # target_table = table_products.first
+      expect(assigns(:products)).to eq products_list
     end
   end
 end
