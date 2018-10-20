8b032e7b90247c67ebd99f17c2350dcc281f9a16
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-10-20 15:48:04 +0900

    Update product_controller_spec
    @related_productsのテストを追加

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -1,23 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Potepan::ProductsController, type: :controller do
-  describe 'Get #show' do
-    let(:konomi) { create :product }
+  describe 'GET #show' do
+    let(:categories) { create(:taxonomy, name: 'Categories') }
+    let(:dinasor) do
+      categories.root.children.create(name: 'Dinasor', taxonomy: categories)
+    end
+    let(:dinasors_list) do
+      %w(big middle small).map do |name|
+        create(:product, name: "#{name}-dinasor") do |product|
+          product.taxons << dinasor
+        end
+      end
+    end
 
     before do
-      get :show, params: { id: konomi.id }
+      get :show, params: { id: dinasors_list.first.id }
     end
 
-    it 'リクエストがsuccessとなること' do
-      expect(response).to be_success
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
     end
 
-    it '@productに適切なプロダクトを割り当てること' do
-      expect(assigns(:product)).to eq konomi
-    end
-
-    it ':showテンプレートを表示すること' do
+    it "renders show template" do
       expect(response).to render_template :show
+    end
+
+    it "assigns @product" do
+      expect(assigns(:product)).to eq dinasors_list.first
+    end
+
+    it "assigns @related_product" do
+      expect(assigns(:related_products)).to eq dinasors_list
     end
   end
 end
