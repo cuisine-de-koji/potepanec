b8d7a989894529b57a844b6f9af17a24cbb6b22e
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-10-24 11:27:08 +0900

    Update cotegories_contoroller.rb(n+1issue) and Clean some files(typo..etc)

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,6 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @products = @taxon.show_products
-    @roots_taxon = Spree::Taxon.roots
+    @root_taxons = Spree::Taxon.roots
   end
 end
--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -3,10 +3,12 @@ Spree::Taxon.class_eval do
     # taxonがrootノードかどうかで表示商品を場合分け
     if root?
       # 子供を持たない末端の葉っぱtaxonsのAR(ActiveRecord::Relation)商品群を取得後,
-      # 重複がなくユニークで且つnilや空白を削除した配列に整形
-      leaves.includes(:products).map(&:products).flatten.compact.uniq
+      # 重複がなくユニークで且つnilや空白を削除した配列に整形。
+      # またN + 1問題を回避するために、事前に@taxon.leavesに関連する:productsとその:productsに関連する:master、そして
+      # その:masterにdelegateされている:default_price(Spree::DefaultPrice)と:image(Spree::Image)をeagerload。
+      leaves.includes(products: { master: [:default_price, :images] }).map(&:products).flatten.compact.uniq
     else
-      products
+      products.includes(master: [:default_price, :images])
     end
   end
 end
