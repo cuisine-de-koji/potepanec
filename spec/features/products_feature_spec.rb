77237c984e245bc4a6154d432fc5662c03f85651
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-10-25 20:33:05 +0900

    Add products_feature_spec

---
+++ spec/features/products_feature_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "product pages", type: :feature do
+  let(:root) { create :taxon }
+  let(:product) { create :product, taxons: [root] }
+
+  scenario "商品の内容が適切である" do
+    visit potepan_product_path(product.id)
+    expect(page).to have_content "一覧ページへ戻る"
+    expect(page).to have_content product.name
+    expect(page).to have_content product.price
+    expect(page).to have_content product.description
+  end
+
+  scenario "click 一覧ページへ戻る" do
+    visit potepan_product_path(product.id)
+    click_on "一覧ページへ戻る"
+    expect(page).to have_content root.name
+    expect(page).to have_current_path(potepan_category_path(root.id))
+  end
+end
