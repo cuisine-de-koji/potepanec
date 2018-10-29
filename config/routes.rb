2edc83be226c5897e5ac3f5a32db7df68a9c28c3
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-10-29 18:11:02 +0900

    Clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  namespace :potepan do
-    get 'homes/index'
-  end
-
   # This line mounts Solidus's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
