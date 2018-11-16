00e10067baeb8a115443b97ba001c41c41bf4835
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-11-16 17:33:42 +0900

    Add model_spec #reject_self,#関連商品が適切に取得できている

--- spec/models/product_decorator_model_spec.rb
+++ spec/models/product_decorator_model_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Spree::Product, type: :model do
-  let(:categories) { create(:taxonomy, name: 'Categories') }
-  let(:dinasor) { categories.root.children.create(name: 'Dinasor', taxonomy: categories) }
+  let!(:categories) { create(:taxonomy, name: 'Categories') }
+  let!(:dinasor) { categories.root.children.create(name: 'Dinasor') }
   let!(:dinasors_list) { create_list(:product, 10, taxons: [dinasor]) }
-  let(:dinasor_1) { dinasors_list.first }
-  let(:dog) { categories.root.children.create(name: 'Dog', taxonomy: categories) }
-  let!(:dog_1) { create(:product, taxons: [dog]) }
+  let!(:dinasor_1) { dinasors_list.first }
+  let!(:dog) { categories.root.children.create(name: 'Dog') }
+  let!(:dog_1) { create(:product, name: 'DOG', taxons: [dog]) }
 
   describe "#related_products" do
     context '関連商品がある場合' do
       let(:dinasor_related_products) { dinasor_1.related_products }
+
+      it '関連商品が適切に取得できている' do
+        expect(dinasor_related_products).to match_array dinasors_list - [dinasor_1]
+      end
 
       it '関連商品は空ではない' do
         expect(dinasor_related_products).not_to be_empty
@@ -39,6 +43,12 @@ RSpec.describe Spree::Product, type: :model do
       it '商品数がlimitの数となる' do
         limit = 8
         expect(Spree::Product.random_and_limitted_items(limit).count).to eq 8
+      end
+    end
+
+    describe "#reject_self" do
+      it '取得した関連商品から詳細商品自身は省かれている' do
+        expect(Spree::Product.reject_self(dinasor_1)).to_not include dinasor_1
       end
     end
   end
