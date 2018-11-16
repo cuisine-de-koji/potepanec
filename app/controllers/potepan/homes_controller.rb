7cc42657cc1d2d7e77bb10689c916f1b8f1191ef
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-11-16 17:53:40 +0900

    Change view of index.html for not to show 新着商品 if @new_products is empty

--- app/controllers/potepan/homes_controller.rb
+++ app/controllers/potepan/homes_controller.rb
@@ -1,7 +1,7 @@
 class Potepan::HomesController < ApplicationController
+  MONTH_FOR_NEW = 1
   def index
     # available_onが一ヶ月以内の商品を新着商品とする
-    @new_products = Spree::Product.where("available_on > ?", Date.today.prev_month(1)).
-      order(available_on: :desc)
+    @new_products = Spree::Product.where("available_on > ?", Date.today.prev_month(MONTH_FOR_NEW)).order(available_on: :desc)
   end
 end
