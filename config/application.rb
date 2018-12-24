a5a1daba9ae56fe4dd2ef93427346dac63169efc
Koji Yamamuro <iq1ooo.midori@gmail.com>
2018-12-24 20:06:03 +0900

    新着商品を6ヶ月以内のものとする

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ Bundler.require(*Rails.groups)
 
 module Potepanec
   class Application < Rails::Application
-
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
