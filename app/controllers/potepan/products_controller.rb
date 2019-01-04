47f824040b677460dbb03a3fe5c56dfaf420adff
Koji Yamamuro <iq1ooo.midori@gmail.com>
2019-01-04 20:44:31 +0900

    Add products_controller#search && Add before_action

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,6 +2,8 @@ class Potepan::ProductsController < ApplicationController
   RELATED_PRODUCTS_NUMS = 8
   VALID_PARAMETER = %i(view sort tshirt-size tshirt-color keyword).freeze
 
+  before_action :set_roots, only: [:index, :search]
+
   def show
     @product = Spree::Product.find(params[:id])
     @images = @product.images
@@ -18,9 +20,20 @@ class Potepan::ProductsController < ApplicationController
     @products = @product_filter.filtered_products
   end
 
+  def search
+    @product_filter = ProductFilter.new(filter_params)
+    @products = @product_filter.filtered_products
+
+    redirect_to potepan_products_url unless params[:keyword].present?
+  end
+
   private
 
-    def filter_params
-      params.permit(*VALID_PARAMETER)
-    end
+  def filter_params
+    params.permit(*VALID_PARAMETER)
+  end
+
+  def set_roots
+    @root_taxons = Spree::Taxon.roots
+  end
 end
