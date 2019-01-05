72d5672f678041d075d8fbbec12b10f1569e6c85
Koji Yamamuro <iq1ooo.midori@gmail.com>
2019-01-06 07:24:57 +0900

    Update cart page && show line items

--- app/controllers/potepan/orders_controller.rb
+++ app/controllers/potepan/orders_controller.rb
@@ -1,13 +1,15 @@
 module Potepan
   class OrdersController < Potepan::StoreController
     def edit
+      @order = current_order
     end
 
     def add_cart
+      @order = current_order(create_order_if_necessary: true)
       variant = Spree::Variant.find(params[:variant_id])
       quantity = params[:quantity].to_i
-      @order = current_order
 
+      @order.content.add(variant, quantify)
 
       binding.pry
       redirect_to potepan_cart_url
