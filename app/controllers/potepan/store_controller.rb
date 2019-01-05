adde2b94cab53715819933986384e8774cd20065
Koji Yamamuro <iq1ooo.midori@gmail.com>
2019-01-05 17:49:39 +0900

    Update StoreController add set_guest_token

--- app/controllers/potepan/store_controller.rb
+++ app/controllers/potepan/store_controller.rb
@@ -1,4 +1,12 @@
 module Potepan
   class StoreController < ApplicationController
+    before_action :set_guest_token
+
+    def set_guest_token
+      unless cookies.signed[:guest_token].present?
+        # browser closed forget token
+        cookies.signed[:guest_token] = SecureRandom.urlsafe_base64(nil, false)
+      end
+    end
   end
 end
